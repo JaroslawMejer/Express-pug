2fc9f019e7370570ec6f76e464bff368e8c66a7b
Jarosław Mejer <jaroslaw.mejer@gmail.com>
2018-07-12 14:26:29 +0200

    Add Google auth & finish the project

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '193477386420-4kamtqogo2vh38fp9qvk5ot3lc008s3m.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'Gmjzgxxi-lwhLCsEB23xZxsC',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
